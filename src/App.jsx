31cd41d70e083a9684729cb35a51d20381a149fc
snowsonbassanio2005-dot <snowsonbassanio2005@gmail.com>
2026-02-14 15:37:45 +0530

    Initial Commit of My Portfolio!

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <main>
+            <div>
+                <h1>Welcome to my Portfolio!</h1>
+            </div>
+        </main>
+    )
+}
+export default App
